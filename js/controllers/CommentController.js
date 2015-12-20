8a0e2aed51f622d61bd9ae8b4f391e342fdf478e
Wasif Zaman <teatocode@gmail.com>
2015-12-20 08:15:27 -0500

    Added sort comments

--- js/controllers/CommentController.js
+++ js/controllers/CommentController.js
@@ -3,7 +3,10 @@ app.controller('CommentController', ['$scope', '$routeParams', 'suggestions', fu
 		$scope.comments = data[$routeParams.index].comments;
 	});
 
-	$scope.newComment = {};
+	$scope.newComment = {
+		upvotes: 0,
+		downvotes: 0
+	};
 
 	$scope.addComment = function() {
 		$scope.comments.push($scope.newComment);
@@ -12,6 +15,7 @@ app.controller('CommentController', ['$scope', '$routeParams', 'suggestions', fu
 
 	$scope.upVote = function(index) {
 		$scope.comments[index].upvotes += 1;
+		$scope.sortComments();
 	};
 
 	$scope.downVote = function(index) {
@@ -19,6 +23,21 @@ app.controller('CommentController', ['$scope', '$routeParams', 'suggestions', fu
 	};
 
 	$scope.sortComments = function() {
+		var doubleArray = [];
+		for (index in $scope.comments) {
+			var upvoteCount = $scope.comments[index].upvotes;
+			doubleArray.push([upvoteCount, $scope.comments[index]]);
+		}
 
+		doubleArray.sort(function(a, b) {
+			if (a[0] < b[0]) { return -1; }
+			if (a[0] > b[0]) { return 1; }
+			return 0;
+		});
+
+		$scope.comments = [];
+		for (var i = doubleArray.length - 1; i >= 0; i--) {
+			$scope.comments.push(doubleArray[i][1]);
+		}
 	};
 }]);
